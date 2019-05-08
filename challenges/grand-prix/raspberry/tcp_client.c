9864ee903a84480ae425a75100762d7cce809ee9
Gera Galindo <liu.gera@gmail.com>
2019-05-08 10:59:06 -0500

    Add Thread handling for multiple messages from Server

--- challenges/grand-prix/raspberry/tcp_client.c
+++ challenges/grand-prix/raspberry/tcp_client.c
@@ -5,7 +5,10 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <wiringPi.h>          
-#include <lcd.h>               
+#include <lcd.h>  
+#include <pthread.h>
+#include <string.h>
+#include <arpa/inet.h>        
  
 //USE WIRINGPI PIN NUMBERS
 #define LCD_RS  25               //Register select pin
@@ -15,45 +18,94 @@
 #define LCD_D6  21               //Data pin 6
 #define LCD_D7  14               //Data pin 7
 
-int main() {
-    printf("Welcome to Raspberry Pi!\n");
-    int lcd;               
-    wiringPiSetup();        
-    lcd = lcdInit (2, 16, 4, LCD_RS, LCD_E, LCD_D4, LCD_D5, LCD_D6, LCD_D7, 0, 0, 0, 0);
+char serverMessage[10];
 
-    // create a socket
-    int networkSocket;
-    networkSocket = socket(AF_INET, SOCK_STREAM, 0); // Using default protocol (TCP)
-
-    // specify an address for the socket
-    struct sockaddr_in serverAddress;
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(9009);
-    serverAddress.sin_addr.s_addr = inet_addr("192.168.1.72");
-
-    // Connect and check for connection errors
-    int connectionStatus = connect(networkSocket, (struct sockaddr *) &serverAddress, sizeof(serverAddress));
-    if(connectionStatus == -1){
-        printf("Error while trying to connect to remote address\n");
-        return 1;
-    }
-
-    // receive data from server
-    char serverResponse[64];
-    /*for(int i = 0; i < 10; ++i){
-	recv(networkSocket, &serverResponse, sizeof(serverResponse), 0);
-	lcdPuts(lcd, serverResponse);
-    }*/
-    recv(networkSocket, &serverResponse, sizeof(serverResponse), 0);
-    lcdPuts(lcd, serverResponse);
-
-    char serverMessage[] = "Raspberry puto";
-    send(networkSocket, serverMessage, sizeof(serverMessage), 0);
-
-    // Close the socket
-    printf("Closing the Socket\n");
-    close(networkSocket);
-    return 0;
+void clearMessage(){
+	for(int i = 0; i < 10; ++i){
+		serverMessage[i] = 0;
+	}
 }
+
+static void * connectionThread(void *arg){
+	int lcd;
+	wiringPiSetup();        
+	lcd = lcdInit (2, 16, 4, LCD_RS, LCD_E, LCD_D4, LCD_D5, LCD_D6, LCD_D7, 0, 0, 0, 0);
+
+	// create a socket
+	int networkSocket;
+	networkSocket = socket(AF_INET, SOCK_STREAM, 0); // Using default protocol (TCP)
+
+	// specify an address for the socket
+	struct sockaddr_in serverAddress;
+	serverAddress.sin_family = AF_INET;
+	serverAddress.sin_port = htons(9009);
+	serverAddress.sin_addr.s_addr = inet_addr("192.168.1.72");
+
+	// Connect and check for connection errors
+	int connectionStatus = connect(networkSocket, (struct sockaddr *) &serverAddress, sizeof(serverAddress));
+	if(connectionStatus == -1){
+		printf("Error while trying to connect to remote address\n");
+		return (void *)1;
+	}
+	
+	while(1){
+		// receive data from server
+		clearMessage();
+		recv(networkSocket, &serverMessage, sizeof(serverMessage), 0);
+		printf("Message Received\n");
+
+		if (!strcmp(serverMessage, "Quit")){
+			printf("Quit\n");
+			lcdClear(lcd);
+			lcdPosition(lcd, 0, 0);
+			lcdPuts(lcd, serverMessage);
+			break;
+		}
+
+		lcdClear(lcd);
+		lcdPosition(lcd, 0, 0);
+		lcdPuts(lcd, serverMessage);
+
+		char clientMessage[] = "Received";
+		send(networkSocket, clientMessage, sizeof(clientMessage), 0);
+		printf("Message Sent\n");
+	}
+	
+
+	// Close the socket
+	printf("Closing the Socket\n");
+	close(networkSocket);
+}
+
+int main() {
+	printf("Welcome to Raspberry Pi!\n");
+	
+	pthread_t clientThread;
+	void *res;
+	int s;
+
+	s = pthread_create(&clientThread, NULL, connectionThread, "Initializing TCP Client Thread");
+        if (s != 0){
+		printf("Error in Thread creation\n");
+	}
+
+	s = pthread_join(clientThread, &res);
+	if (s != 0){
+		printf("Error in Thread joining\n");
+	}
+
+	printf("Thread Joined\n");
+	
+	return 0;
+}
+
+
+
+
+
+
+
+
+
 
 
